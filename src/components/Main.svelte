d801c63b820968895d0e465825a4dec6472ab344
Yessica Ríos <yskrios@gmail.com>
2021-04-13 19:43:26 -0500

    configuracion del proyecto

---
+++ src/components/Main.svelte
@@ -0,0 +1,27 @@
+<script></script>
+
+<main class="Main">
+  <div class="Main-container">
+    <div class="main-content">
+      <slot />
+      <div class="sidebar">sidebar</div>
+    </div>
+  </div>
+</main>
+
+<style>
+  .Main-container {
+    display: grid;
+    grid-template-columns: (auto, 1024px);
+    justify-content: center;
+    align-items: center;
+  }
+
+  .main-content {
+    margin: 2em;
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap: 60px;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+  import Main from "../components/Main.svelte";
+  export let segment;
+</script>
+
+<Nav {segment} />
+
+<Main>
+  <slot />
+</Main>
